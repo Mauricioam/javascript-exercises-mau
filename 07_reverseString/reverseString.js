c92bd4809747fa2f1d9a7ed5c05909432b2672c6
Mauricioam <mauricioam91@gmail.com>
2025-09-20 20:17:59 -0300

    exercise 7 resolved

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let splited = str.split("");
+  let result = [];
+  for (let i = splited.length - 1; i >= 0; i--) {
+    result.push(splited[i]);
+  }
+  return result.join("");
 };
 
 // Do not edit below this line
