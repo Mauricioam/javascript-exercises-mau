6ca5bc29ed5d3581f39549dc117d776a365ba07a
Mauricioam <mauricioam91@gmail.com>
2025-09-20 17:33:57 -0300

    exercise resolved

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let result = [];
+  while (num > 0) {
+    result.push(string);
+    num--;
+  }
+  return result.join("");
 };
 
 // Do not edit below this line
