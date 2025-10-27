d835e3459b106693e2a54ff2fdcd48765cd5626d
Mauricioam <mauricioam91@gmail.com>
2025-10-27 09:24:36 -0300

    excercise 09 resolved

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (startNum, endNum) {
+  if (isNaN(startNum) || isNaN(endNum) || startNum < 0 || endNum < 0) {
+    return "ERROR";
+  }
+  if (startNum > endNum) {
+    let interMedValue;
+    interMedValue = startNum;
+    startNum = endNum;
+    endNum = interMedValue;
+  }
+  let sum = 0;
+  for (let i = startNum; i <= endNum; i++) {
+    sum = sum + i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
