1d221f7f839731a218437a4c1ab5f449864f773c
Mauricioam <mauricioam91@gmail.com>
2025-11-11 21:52:14 -0300

    added fx to clean punctuation

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const alfanumericRef = "abcdefghijklmnopqrstuxyz1234567890";
+  let cleanedString = string
+    .split("")
+    .filter((letter) => alfanumericRef.includes(letter))
+    .join("");
 };
 
 // Do not edit below this line
